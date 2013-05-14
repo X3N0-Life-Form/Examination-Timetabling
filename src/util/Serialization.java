bc336831c25a43e474860d5c388fac965dcf3ee3
X3N0-Life-Form <nope@laptop.here>
2013-05-14 11:00:58 +0200

    added a few prints + documented the Serialization class.

--- src/util/Serialization.java
+++ src/util/Serialization.java
@@ -9,9 +9,24 @@ import java.io.ObjectOutputStream;
 
 import struct.Solution;
 
+/**
+ * Contains various methods that related to object serialization.
+ * @author Adrien Droguet - Sara Tari
+ * @see Solution
+ */
 public class Serialization {
+	
+	/**
+	 * Loads a Solution from the specified file path.
+	 * @param serializedName File path
+	 * @return A Solution object.
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	public static Solution loadSolution(String serializedName) throws FileNotFoundException, IOException,
 	ClassNotFoundException {
+		System.out.println("Loading serialized Solution " + serializedName);
 		FileInputStream fis = new FileInputStream(serializedName);
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		Solution toLoad = (Solution) ois.readObject();
@@ -20,8 +35,16 @@ public class Serialization {
 		return toLoad;
 	}
 
+	/**
+	 * Serializes a Solution object into the specified file path.
+	 * @param toSave Solution object to save.
+	 * @param serializedName File path
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public static void saveSolution(Solution toSave, String serializedName) throws FileNotFoundException,
 	IOException {
+		System.out.println("Serializing solution into " + serializedName);
 		FileOutputStream fos = new FileOutputStream(serializedName);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
 		oos.writeObject(toSave);
